daec096f80f6f9ed364f0dbe976419c43a81f393
zhouli <563287128@qq.com>
2019-12-08 01:28:54 +0800

    详情页信息添加（差评论）

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,9 +1,10 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
-import { AtAvatar } from 'taro-ui'
+import { AtAvatar, AtIcon } from 'taro-ui'
 import { connect } from '@tarojs/redux'
 import action from '../../utils/action'
 import { getPicDetail } from '../../api'
+import moment from '../../utils/moment'
 
 import './index.scss'
 import { bindShow } from '../../utils/style'
@@ -52,6 +53,7 @@ class Index extends Component {
 
     render() {
         const { isLoading, picInfo } = this.state
+        const { exifInfo, category } = picInfo
         return isLoading ? (
             <View>loading...</View>
         ) : (
@@ -99,18 +101,121 @@ class Index extends Component {
                 <View className='detail__page__detail'>
                     <View className='detail__page__detail__basic'>
                         <View className='detail__page__detail__heat'>
-                            <Text>热度</Text>
-                            <Text>{picInfo.ratingMax}</Text>
-                            <Text>{picInfo.ratingMaxDate}</Text>
+                            <Text className='detail__page__detail__heat__title'>
+                                热度
+                            </Text>
+                            <Text className='detail__page__detail__heat__content'>
+                                {parseFloat(picInfo.ratingMax).toFixed(1)}
+                            </Text>
+                            <Text className='detail__page__detail__time'>
+                                {moment(picInfo.ratingMaxDate).format(
+                                    'YYYY.MM.DD'
+                                )}
+                            </Text>
                         </View>
                         <View className='detail__page__detail__pv'>
-                            <Text>浏览量</Text>
+                            <Text className='detail__page__detail__pv__title'>
+                                浏览量
+                            </Text>
                             <Text>{picInfo.picturePvCount}</Text>
+                            <View className='detail__page__detail__placeholder'>
+                                {' '}
+                            </View>
                         </View>
                         <View className='detail__page__detail__like'>
-                            <Text>点赞数</Text>
+                            <Text className='detail__page__detail__like__title'>
+                                点赞数
+                            </Text>
                             <Text>{picInfo.pictureLikeedCount}</Text>
+                            <View className='detail__page__detail__placeholder'>
+                                {' '}
+                            </View>
                         </View>
+                        <View className='detail__page__detail__hot'>
+                            <Text className='detail__page__detail__hot__title'>
+                                热门
+                            </Text>
+                            <AtIcon
+                                value='star-2'
+                                size='18'
+                                color='#525558'
+                            ></AtIcon>
+                            <Text className='detail__page__detail__time'>
+                                {moment(picInfo.riseUpDate).format(
+                                    'YYYY.MM.DD'
+                                )}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                <View className='detail__page__camera'>
+                    <Text className='detail__page__camera__title'>
+                        {exifInfo.modelVcg}
+                    </Text>
+                    <Text className='detail__page__camera__subtitle'>
+                        {exifInfo.lens}
+                    </Text>
+                    <View className='detail__page__camera__flex'>
+                        <View className='detail__page__camera__flex__item'>
+                            <Image
+                                className='detail__page__camera__flex__item__img'
+                                src='https://cdn.500px.me/images/photoDetail/aperture.svg'
+                            ></Image>
+                            <Text>光圈 {exifInfo.aperture}</Text>
+                        </View>
+                        <View className='detail__page__camera__flex__item'>
+                            <Image
+                                className='detail__page__camera__flex__item__img'
+                                src='https://cdn.500px.me/images/photoDetail/time.svg'
+                            ></Image>
+                            <Text>
+                                快门速度 {`${exifInfo.exposureTimeVcg} s`}
+                            </Text>
+                        </View>
+                        <View className='detail__page__camera__flex__item'>
+                            <Image
+                                className='detail__page__camera__flex__item__img'
+                                src='https://cdn.500px.me/images/photoDetail/focus.svg'
+                            ></Image>
+                            <Text>焦距 {exifInfo.focalLength}</Text>
+                        </View>
+                        <View className='detail__page__camera__flex__item'>
+                            <Image
+                                className='detail__page__camera__flex__item__img'
+                                src='https://cdn.500px.me/images/photoDetail/iso.svg'
+                            ></Image>
+                            <Text>{`ISO ${exifInfo.iso}`}</Text>
+                        </View>
+                    </View>
+                </View>
+                <View className='detail__page__class'>
+                    <View className='detail__page__class__line'>
+                        <Text className='detail__page__class__line__title'>
+                            分类
+                        </Text>
+                        <Text className='detail__page__class__line__content'>
+                            {category.name}
+                        </Text>
+                    </View>
+                    <View className='detail__page__class__line'>
+                        <Text className='detail__page__class__line__title'>
+                            上传时间
+                        </Text>
+                        <Text className='detail__page__class__line__content'>
+                            {moment(picInfo.uploadedDate).format(
+                                'YYYY.MM.DD HH:mm:ss'
+                            )}
+                        </Text>
+                    </View>
+                    <View className='detail__page__class__line'>
+                        <Text className='detail__page__class__line__title'>
+                            拍摄时间
+                        </Text>
+                        <Text className='detail__page__class__line__content'>
+                            {moment(exifInfo.dateTime).format(
+                                'YYYY.MM.DD HH:mm:ss'
+                            )}
+                        </Text>
                     </View>
                 </View>
             </View>
